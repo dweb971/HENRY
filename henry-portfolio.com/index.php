64d8df90d45d0bd331529fae17aeb32a6d36b13d
HENRY Jonathan <rsma.simplon21@gmail.com>
2021-04-08 08:52:08 -0400

    portfolio henry

---
+++ henry-portfolio.com/index.php
@@ -0,0 +1,282 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,
+      shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+      integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+      crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>HENRY portfolio</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-1">
+          <img src="decoupes/testimonial/avatar/avatar.png" alt="avatar">
+
+        </div>
+        <div class="col-2">
+          <p class="marge-profil"><span class="gras">Lewis <br>
+            Nathaniel</span><br>
+            <span class="gris">UI & UX designer</span></p>
+        </div>
+        <div class="col-9">
+          <ul class="nav nav-h marge-droite">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">WORK</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">ABOUT ME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">BLOG</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CONTACT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link btn contour-noir" href="#">HIRE ME</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6 marge-presentation">
+          <h1><span class="gris-clair">Hello,I'm</span><br>
+            <span class="gras">Lewis Nathaniel</span><br>
+            <span class="gris">UI & UX designer</span></h1>
+          <div class="row">
+            <div class="col-6">
+              <a class="nav-link btn bouton-noir blanc" href="#">HIRE ME</a>
+            </div>
+            <div class="col-6">
+              <a class="nav-link btn contour-noir" href="#">SEE MY RESUME</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 droite">
+          <img src="decoupes/top/droite/me.png" class="bg-image"
+            alt="me">
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+
+        </div>
+        <div class="col-4">
+          <ul class="nav nav-h">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">All</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">App</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Website</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Interactions</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-4">
+          
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <img src="decoupes/works/cover_photo.png" alt="photo travail">
+          <div class="row">
+            <div class="col-10">
+              <a href="http://"></a>
+              <p>Category<br>
+              PROJECT TITLE</p>
+            </div>
+            <div class="col-2">
+              2018
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="decoupes/works/cover_photo.png" alt="photo travail">
+          <div class="row">
+            <div class="col-10">
+              <p>Category<br>
+              PROJECT TITLE</p>
+            </div>
+            <div class="col-2">
+              2018
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="decoupes/works/cover_photo.png" alt="photo travail">
+          <div class="row">
+            <div class="col-10">
+              <p>Category<br>
+              PROJECT TITLE</p>
+            </div>
+            <div class="col-2">
+              2018
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <img src="decoupes/works/cover_photo.png" alt="photo travail">
+          <div class="row">
+            <div class="col-10">
+              <p>Category<br>
+              PROJECT TITLE</p>
+            </div>
+            <div class="col-2">
+              2018
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="decoupes/works/cover_photo.png" alt="photo travail">
+          <div class="row">
+            <div class="col-10">
+              <p>Category<br>
+              PROJECT TITLE</p>
+            </div>
+            <div class="col-2">
+              2018
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="decoupes/works/cover_photo.png" alt="photo travail">
+          <div class="row">
+            <div class="col-10">
+              <p>Category<br>
+              PROJECT TITLE</p>
+            </div>
+            <div class="col-2">
+              2018
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <img src="decoupes/works/cover_photo.png" alt="photo travail">
+          <div class="row">
+            <div class="col-10">
+              <p>Category<br>
+              PROJECT TITLE</p>
+            </div>
+            <div class="col-2">
+              2018
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="decoupes/works/cover_photo.png" alt="photo travail">
+          <div class="row">
+            <div class="col-10">
+              <p>Category<br>
+              PROJECT TITLE</p>
+            </div>
+            <div class="col-2">
+              2018
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <img src="decoupes/works/cover_photo.png" alt="photo travail">
+          <div class="row">
+            <div class="col-10">
+              <p>Category<br>
+              PROJECT TITLE</p>
+            </div>
+            <div class="col-2">
+              2018
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-5">
+
+        </div>
+        <div class="col-2">
+          <a class="nav-link btn contour-noir" href="#">Load more work</a>
+        </div>
+        <div class="col-5">
+
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <img src="decoupes/About_me/me.png" alt="me">
+        </div>
+        <div class="col-6">
+          <h1><span class="gras"></span> ABOUT ME<br>
+          
+         <span class="gris-clair agrandissement">WHO AM I</span></h1>
+          <p class="gris-clair">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut sapien neque. Vivamus dignissim fringilla ligula, ut bibendum lorem varius in. Aenean a feugiat ligula. Sed non cursus elit, consequat dignissim neque. Quisque ac sollicitudin augue. Etiam leo mi, convallis sit amet gravida non, sodales at nunc. Donec at nisl id ante dictum egestas sit amet in lectus. In ultrices eros arcu, nec fringilla neque cursus lacinia. Vestibulum eu sem quis libero semper imperdiet. Praesent sollicitudin commodo odio porttitor laoreet. </p>
+          <div class="row">
+            <div class="col-6">
+              <a class="nav-link btn bouton-noir blanc" href="#">HIRE ME</a>
+            </div>
+            <div class="col-6">
+              <a class="nav-link btn contour-noir" href="#">SEE MY RESUME</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row noir">
+        <div class="col-3">
+
+        </div>
+        <div class="col-6 marge-haut-bas">
+        <p class="blanc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut sapien neque. Vivamus dignissim fringilla ligula, ut bibendum lorem varius in. Aenean a feugiat ligula. Sed non cursus elit, consequat dignissim neque. Quisque ac sollicitudin augue. Etiam leo mi, convallis sit amet gravida non, sodales at nunc. Donec at nisl id ante dictum egestas sit amet in lectus. In ultrices eros arcu, nec fringilla neque cursus lacinia. Vestibulum eu sem quis libero semper imperdiet. Praesent sollicitudin commodo odio porttitor laoreet. </p>
+        </div>
+        <div class="col-3">
+
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-5">
+
+        </div>
+        <div class="col-2">
+          <img src="decoupes/testimonial/avatar/avatar.png" class="centre" alt="photo de profil">
+        </div>
+        <div class="col-5">
+
+        </div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+      crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
